f1d129ea8621d900003d75f29c2c3c2970fe83c2
Makoto Morita <mmakmo@gmail.com>
2012-03-12 23:32:46 +0900

    Descrived specifications

--- src/main/java/answers/Answer1.java
+++ src/main/java/answers/Answer1.java
@@ -1,5 +1,10 @@
 package answers;
 
+/*
+ 足し算
+2つの整数を足しあわせ、整数の答えを求める。
+
+*/
 public interface Answer1 {
 	int plus(int i, int j);
 }
--- src/main/java/answers/Answer3.java
+++ src/main/java/answers/Answer3.java
@@ -1,5 +1,17 @@
 package answers;
 
+/*
+FizzBuzz
+
+指定した正の整数が
+- 3で割り切れる場合は "Fizz"
+- 5で割り切れる場合は "Buzz"
+- 3と5で割り切れる場合は"Fizz Buzz"
+- 3でも5でも割り切れない場合は、その数字
+を文字列として答えを返す。
+
+*/
+
 public interface Answer3 {
 
 	String fizzBuzz(int i);
