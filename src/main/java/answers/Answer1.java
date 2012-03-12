68827b35875fb9a1df4a3f82fa285551caeebb21
irof <@irof>
2012-03-13 01:10:26 +0900

    convert to javadoc comment

--- src/main/java/answers/Answer1.java
+++ src/main/java/answers/Answer1.java
@@ -1,10 +1,11 @@
 package answers;
 
-/*
- 足し算
-2つの整数を足しあわせ、整数の答えを求める。
-
-*/
+/**
+ * 足し算
+ * 
+ * 2つの整数を足しあわせ、整数の答えを求める。
+ */
 public interface Answer1 {
+
 	int plus(int i, int j);
 }
--- src/main/java/answers/Answer3.java
+++ src/main/java/answers/Answer3.java
@@ -1,17 +1,11 @@
 package answers;
 
-/*
-FizzBuzz
-
-指定した正の整数が
-- 3で割り切れる場合は "Fizz"
-- 5で割り切れる場合は "Buzz"
-- 3と5で割り切れる場合は"Fizz Buzz"
-- 3でも5でも割り切れない場合は、その数字
-を文字列として答えを返す。
-
-*/
-
+/**
+ * FizzBuzz
+ * 
+ * 指定した正の整数が 3で割り切れる場合は "Fizz" 5で割り切れる場合は "Buzz" 3と5で割り切れる場合は"FizzBuzz"
+ * 3でも5でも割り切れない場合は、その数字 を文字列として答えを返す。
+ */
 public interface Answer3 {
 
 	String fizzBuzz(int i);
