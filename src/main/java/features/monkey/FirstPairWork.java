c65527ce0ac1445578246a8c4fc54fb2544eddd4
Kamiya Atsuteru <teru.k.business@gmail.com>
2012-08-05 17:51:55 +0900

    切なる願い

---
+++ src/main/java/features/monkey/FirstPairWork.java
@@ -0,0 +1,17 @@
+package features.monkey;
+
+public class FirstPairWork implements features.FirstPairWork {
+
+	@Override
+	public String getIkigomi() {
+		// TODO Auto-generated method stub
+		return "8/18がんばるぞ？";
+	}
+
+	@Override
+	public String getMembers() {
+		// TODO Auto-generated method stub
+		return "@kami_teru,sign please!,sign please!,...";
+	}
+
+}
