ab5395ceacbb0eb3d928873b369aed5dae00a733
Kamiya Atsuteru <teru.k.business@gmail.com>
2012-08-14 15:13:19 +0900

    .ssh/config のHost設定でgithubのUserを記述しなかったらどうなるかを確かめるテスト

--- src/main/java/features/monkey/FirstPairWork.java
+++ src/main/java/features/monkey/FirstPairWork.java
@@ -11,7 +11,7 @@ public class FirstPairWork implements features.FirstPairWork {
 	@Override
 	public String getMembers() {
 		// TODO Auto-generated method stub
-		return "@kami_teru,sign please!,sign please!,...";
+		return "@kami_teru,sign please!,sign please!,sign please!,..";
 	}
 
 }
