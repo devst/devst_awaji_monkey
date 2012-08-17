5afdafb38bb5831d9cf650df7e9ee3c28bde5b02
Kamiya Atsuteru <teru.k.business@gmail.com>
2012-08-18 02:56:23 +0900

    clear FirstPairWork

--- src/main/java/features/monkey/FirstPairWork.java
+++ src/main/java/features/monkey/FirstPairWork.java
@@ -5,13 +5,13 @@ public class FirstPairWork implements features.FirstPairWork {
 	@Override
 	public String getIkigomi() {
 		// TODO Auto-generated method stub
-		return "8/18がんばるぞ！";
+		return null;
 	}
 
 	@Override
 	public String getMembers() {
 		// TODO Auto-generated method stub
-		return "@kami_teru,sign please!,sign please!,sign please!,..";
+		return null;
 	}
 
 }
